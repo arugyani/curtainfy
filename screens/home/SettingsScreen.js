708c32e3bf1e5e253064f76ae32bdfd97c5c324e
Aru Gyani <gyaniaru@gmail.com>
2022-09-25 01:30:28 -0500

    sleep

--- screens/home/SettingsScreen.js
+++ screens/home/SettingsScreen.js
@@ -1,19 +1,28 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, TextInput, View, Pressable, Switch, List, ViewPagerAndroidComponent } from "react-native";
-
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Pressable,
+  Switch,
+  List,
+  ViewPagerAndroidComponent,
+} from "react-native";
 
 import HideKeyboard from "../../components/HideKeyboard";
 import BottomNavigation from "../../components/BottomNavigation";
 import RadioNav from "../../components/RadioNav";
 
-const SwitchComponent = (item => {
-  const [switchState, setSwitchState] = useState(item.state)
+const SwitchComponent = (item) => {
+  const [switchState, setSwitchState] = useState(item.state);
   return (
-
-    <Switch value={switchState} onValueChange={() => setSwitchState(prevState => !prevState)} />
-
-  )
-});
+    <Switch
+      value={switchState}
+      onValueChange={() => setSwitchState((prevState) => !prevState)}
+    />
+  );
+};
 
 const SettingsScreen = ({ navigation, route }) => {
   const [name, changeName] = useState("");
@@ -24,131 +33,90 @@ const SettingsScreen = ({ navigation, route }) => {
         <View style={styles.listContainer}>
           <View style={styles.switchComp}>
             <Text style={styles.switchTxt}>do not disturb</Text>
-            <SwitchComponent style={styles.actualSwitch} item={{ state: false }} />
+            <SwitchComponent
+              style={styles.actualSwitch}
+              item={{ state: false }}
+            />
           </View>
           <View style={styles.switchComp}>
             <Text style={styles.switchTxt}>beta features</Text>
-            <SwitchComponent style={styles.actualSwitch} item={{ state: false }} />
+            <SwitchComponent
+              style={styles.actualSwitch}
+              item={{ state: false }}
+            />
           </View>
-          <View style={styles.switchComp}>
-            <Text style={styles.switchTxt}>notificaiton hours</Text>
-            <TextInput
-            style={styles.listInput}
-            onChangeText={changeName}
-            value={name}
-          />
-          <Text style={styles.dash}>-</Text>
-          <TextInput
-            style={styles.listInput}
-            onChangeText={changeName}
-            value={name}
-          />
+          <View style={styles.switchCompText}>
+            <Text style={styles.switchTxtComp}>notification hours</Text>
+            <View style={styles.inputWrapper}>
+              <TextInput
+                style={styles.listInput}
+                onChangeText={changeName}
+                value={name}
+              />
+              <Text style={styles.dash}>-</Text>
+              <TextInput
+                style={styles.listInput}
+                onChangeText={changeName}
+                value={name}
+              />
+            </View>
           </View>
-          <View style={styles.switchComp}>
-            <Text style={styles.switchTxt}>notifications</Text>
-            <Text style={styles.actualSwitch}>{'>'}</Text>
-          </View>
-          <View style={styles.switchComp}>
-            <Text style={styles.switchTxt}>privacy</Text>
-            <Text style={styles.actualSwitch}>{'>'}</Text>
-          </View>
-          <View style={styles.switchComp}>
-            <Text style={styles.switchTxt}>security</Text>
-            <Text style={styles.actualSwitch}>{'>'}</Text>
-          </View>
-          <View style={styles.switchComp}>
-            <Text style={styles.switchTxt}>help</Text>
-            <Text style={styles.actualSwitch}>{'>'}</Text>
-          </View>
-          <View style={styles.switchComp}>
-            <Text style={styles.switchTxt}>my home</Text>
-            <Text style={styles.actualSwitch}>{'>'}</Text>
-          </View>
-          <View style={styles.switchComp}>
-            <Text style={styles.switchTxt}>about</Text>
-            <Text style={styles.actualSwitch}>{'>'}</Text>
-          </View>
-          
         </View>
-        <BottomNavigation navigation={navigation} route={route}/>
-        </View>
-        
-    </HideKeyboard >
+        <BottomNavigation navigation={navigation} route={route} />
+      </View>
+    </HideKeyboard>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    paddingTop: 50,
     justifyContent: "center",
     alignItems: "center",
   },
 
-  headerWrapper: {
-    justifyContent: "center",
-    alignItems: "center",
-    width: "100%",
-  },
   listContainer: {
-    flex: 1,
-    paddingTop: 50,
-    justifyContent: "end",
-    alignItems: "end",
-  },
-
-  header: {
-    fontSize: "36px",
-    justifyContent: "center",
-    textAlign: "center",
+    height: "100%",
+    marginTop: '45%',
+    justifyContent: "start",
+    alignItems: "center",
   },
 
   switchComp: {
-    flexDirection: "row",
-    flex: 1,
-    justifyContent: "end",
-    alignItems: "start",
-    paddingRight: 20,
-  },
-  switchTxt: {
-    flexDirection: "row",
-    flex: 1,
-    fontSize: 36
-  },
-  actualSwitch: {
+    flexDirection: 'row',
     justifyContent: "center",
     alignItems: "center",
-    paddingTop: 10,
-    fontSize: "36px"
+    marginBottom: 30,
   },
 
-  bold: {
+  switchTxt: {
+    fontSize: 30,
     fontWeight: "700",
+    marginBottom: 12,
+    marginRight: 24,
   },
 
-  input: {
-    height: 100,
-    margin: 20,
-    width: "75%",
-    backgroundColor: "#e4e4e4",
-    paddingHorizontal: 24,
-    fontWeight: "700",
-    borderRadius: 15,
-    padding: 10,
-    fontSize: 100,
-    textAlign: "center",
+  switchTxtComp: {
+    fontSize: 30,
+    fontWeight: '700',
+    marginBottom: 24,
   },
+
+  inputWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
   listInput: {
-    height: 20,
-    margin: 5,
-    width: "10%",
-    backgroundColor: "#e4e4e4",
-    paddingHorizontal: 10,
-    fontWeight: "700",
-    borderRadius: 15,
-    padding: 10,
-    fontSize: 20,
-    textAlign: "center",
+    height: 45,
+    width: 100,
+    marginHorizontal: 12,
+    textAlign: 'center',
+    borderRadius: 50,
+    borderColor: '#000',
+    borderWidth: 1,
+    fontWeight: '700',
   },
 
   navigation: {
